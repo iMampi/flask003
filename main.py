9cdde89ba4dc6c3ab34921524b56402fb4b2fd73
Koytcha <mampiisaotaku@gmail.com>
2021-02-18 16:51:34 +0300

    commit 002

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask.templating import render_template_string
 
 app=Flask(__name__)
@@ -6,11 +6,14 @@ app=Flask(__name__)
 posts=[{
     "date":"14/02/2021",
     "author":"iMampi",
-    "post":"C'est la St-Valentin"
+    "post":"C'est la St-Valentin.",
+    "title":"<3"
     },
     {"date":"18/02/2021",
     "author":"iMampi",
-    "post":"j'ai cru mourrir"
+    "post":"j'ai cru mourrir.",
+    "title":"ill"
+
     }
 ]
 
@@ -18,11 +21,11 @@ posts=[{
 @app.route("/")
 @app.route("/home/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about/")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title="About")
 
 if __name__=='__main__':
     app.run(debug=True)
