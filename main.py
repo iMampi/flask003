876222baa3f11c6ef576aa163f875c75f2dd51f3
iMampi <mampiisaotaku@gmail.com>
2021-02-19 21:50:50 +0300

    commit 004

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import datetime as dt
 from flask import Flask, render_template, url_for,redirect,flash
+from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template_string
+from sqlalchemy.orm import backref
 from forms import RegistrationForm,LoginForm
 
 app=Flask(__name__)
 
+
 #i used secrets.token_hex(16) to create a random secret_key
 app.config['SECRET_KEY'] = '1bc511ffb35deac56b1cf85b5e09f083'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+
+db=SQLAlchemy(app)
+
 posts=[{
     "date":"14/02/2021",
     "author":"iMampi",
@@ -20,6 +28,28 @@ posts=[{
     }
 ]
 
+class User(db.Model):
+    __tablename__='user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True,nullable=False)
+    email = db.Column(db.String(120),unique=True,nullable=False)
+    avatar_file = db.Column(db.String(20),nullable=False,default="default.jpg")
+    password = db.Column(db.String(60),nullable=False)
+    posts = db.relationship('Post',backref='author',lazy=True)
+
+    def __repr__():
+        return f"User('{self.username}','{self.email}','{self.avatar_file}')"
+
+class Post(db.Model):
+    __tablename__='post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120),nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False,default=dt.datetime.utcnow)
+    post = db.Column(db.Text,nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__():
+        return f"User('{self.title}','{self.date_posted}')"
 
 @app.route("/")
 @app.route("/home/")
